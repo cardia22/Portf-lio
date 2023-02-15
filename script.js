5eaf6e7c75ccb7fb959430e21a03cc4b3ee027a3
Gabriel Cardia <bieloliveira01@hotmail.com>
2023-02-14 22:37:49 -0300

    changes about

--- script.js
+++ script.js
@@ -77,238 +77,244 @@ menu.addEventListener("click", function () {
   }, 4000);
 });
 
-
 //translate page
 let translated = false;
-const translateBtn = document.getElementById("translate")
-translateBtn.addEventListener("click", function(){
+const translateBtn = document.getElementById("translate");
+translateBtn.addEventListener("click", function () {
   if (translated) {
     // revert the translations to the original state
-    translateBtn.innerText = "EN"
-    const i1 = document.createElement("i")
-    i1.className = "fa-solid" + " " + "fa-earth-americas"
-    i1.style.marginLeft = "5px"
-    translateBtn.appendChild(i1)
+    translateBtn.innerText = "EN";
+    const i1 = document.createElement("i");
+    i1.className = "fa-solid" + " " + "fa-earth-americas";
+    i1.style.marginLeft = "5px";
+    translateBtn.appendChild(i1);
 
-    const contactHeader = document.getElementById("liveToastBtn")
-    contactHeader.innerText = "Contato" 
-    const i = document.createElement("i")
-    i.className = "fa-solid" + " " + "fa-phone"
-    i.style.marginLeft = "5px"
-    contactHeader.appendChild(i)
+    const contactHeader = document.getElementById("liveToastBtn");
+    contactHeader.innerText = "Contato";
+    const i = document.createElement("i");
+    i.className = "fa-solid" + " " + "fa-phone";
+    i.style.marginLeft = "5px";
+    contactHeader.appendChild(i);
 
-    const aboutTitle = document.getElementById("aboutTitle")
-    aboutTitle.innerText = "Sobre"
+    const aboutTitle = document.getElementById("aboutTitle");
+    aboutTitle.innerText = "Sobre";
 
-    const aboutText = document.getElementById("aboutText")
-    aboutText.innerText = "Olá, meu nome é Gabriel Cardia, tenho 20 anos. Atualmente, estou no 3º Semestre estudando Análise e Desenvolvimento de Sistemas. Sou apaixonado por programação e estou sempre aprendendo. Meu objetivo é se tornar um desenvolvedor FullStack. Faço cursos online, como o FullStack da OneBitcode e o Udemy: Complete Web Development - 20 cursos + 20 projetos."
 
-    const titleSkills = document.getElementById("titleSkills")
+    const aboutText = document.getElementById("aboutText");
+    aboutText.innerHTML =
+    "Olá, meu nome é Gabriel Cardia e eu tenho 20 anos. Atualmente, estou no 3º semestre do curso de Análise e Desenvolvimento de Sistemas e sou apaixonado por programação. Estou em constante aprendizado e pretendo me tornar um desenvolvedor FullStack. <br/><br/> Faço cursos online para aprimorar minhas habilidades, sendo eles:<br/><strong>- Udemy: Desenvolvimento Web Completo - 20 cursos + 20 projetos</strong><br/><strong>- FullStack da OneBitCode</strong>";
+    
+    const titleSkills = document.getElementById("titleSkills");
 
-    titleSkills.innerText = "Minhas Skills"
+    titleSkills.innerText = "Minhas Skills";
 
-    const titleProjects = document.getElementById("titleProjects")
+    const titleProjects = document.getElementById("titleProjects");
 
-    titleProjects.innerText = "Projetos"
+    titleProjects.innerText = "Projetos";
 
-    const calculator = document.getElementById("calculator")
-    calculator.innerText = "Calculadora"
+    const calculator = document.getElementById("calculator");
+    calculator.innerText = "Calculadora";
 
-    const clock= document.getElementById("clock")
-    clock.innerText = "Relógio Digital"
+    const clock = document.getElementById("clock");
+    clock.innerText = "Relógio Digital";
 
-    const killRick = document.getElementById("killRick")
-    killRick.innerText = "Jogo - Mate Rick"
+    const killRick = document.getElementById("killRick");
+    killRick.innerText = "Jogo - Mate Rick";
 
     const projectLinks = document.querySelectorAll(".projectLink");
 
-    projectLinks.forEach(link => {
-      link.textContent = "Link do Projeto"
+    projectLinks.forEach((link) => {
+      link.textContent = "Link do Projeto";
     });
-     
+
     const underConstruction = document.querySelectorAll(".underConstruction");
-  
-    underConstruction.forEach(text => {
-      text.textContent = "Em construção"
+
+    underConstruction.forEach((text) => {
+      text.textContent = "Em construção";
     });
 
-    const contacInformation = document.getElementById("contacInformation")
-    contacInformation.innerText ="Informaçãoes de contato:"
+    const contacInformation = document.getElementById("contacInformation");
+    contacInformation.innerText = "Informaçãoes de contato:";
 
-    const name = document.getElementById("name")
-    name.innerText = "Nome: Gabriel Cardia"
+    const name = document.getElementById("name");
+    name.innerText = "Nome: Gabriel Cardia";
 
     translated = false;
   } else {
     // translate to English
-    translateBtn.innerText = "PT"
-    const i1 = document.createElement("i")
-    i1.className = "fa-solid" + " " + "fa-earth-americas"
-    i1.style.marginLeft = "5px"
-    translateBtn.appendChild(i1)
-    const contactHeader = document.getElementById("liveToastBtn")
-    contactHeader.innerText = "Contact" 
-    const i = document.createElement("i")
-    i.className = "fa-solid" + " " + "fa-phone"
-    i.style.marginLeft = "5px"
-    contactHeader.appendChild(i)
+    translateBtn.innerText = "PT";
+    const i1 = document.createElement("i");
+    i1.className = "fa-solid" + " " + "fa-earth-americas";
+    i1.style.marginLeft = "5px";
+    translateBtn.appendChild(i1);
+    const contactHeader = document.getElementById("liveToastBtn");
+    contactHeader.innerText = "Contact";
+    const i = document.createElement("i");
+    i.className = "fa-solid" + " " + "fa-phone";
+    i.style.marginLeft = "5px";
+    contactHeader.appendChild(i);
 
-    const aboutTitle = document.getElementById("aboutTitle")
-    aboutTitle.innerText = "About"
+    const aboutTitle = document.getElementById("aboutTitle");
+    aboutTitle.innerText = "About";
 
-    const aboutText = document.getElementById("aboutText")
-    aboutText.innerText = "Hello, my name is Gabriel Cardia, I am 20 years old. Currently, I am in the 3rd Semester studying Analysis and Development of Systems. I am passionate about programming and am constantly learning. I aim to become a FullStack developer. I take online courses, such as FullStack from OneBitcode and Udemy: Complete Web Development - 20 courses + 20 projects."
-    const titleSkills = document.getElementById("titleSkills")
+    const aboutText = document.getElementById("aboutText");
+   aboutText.innerHTML = "Hello, my name is Gabriel Cardia and I'm 20 years old. Currently, I'm in the 3rd semester of the Analysis and Development of Systems course and I'm passionate about programming. I'm constantly learning and aiming to become a FullStack developer.<br><br>I take online courses to improve my skills, including:<br/><strong>- Udemy: Complete Web Development - 20 courses + 20 projects</strong><br/><strong>- OneBitCode's FullStack course</strong>";
 
-    titleSkills.innerText = "My skills"
-  
-    const titleProjects = document.getElementById("titleProjects")
-  
-    titleProjects.innerText = "Projects"
-  
-    const calculator = document.getElementById("calculator")
-    calculator.innerText = "Calculator"
-  
-    const clock= document.getElementById("clock")
-    clock.innerText = "Digital Watch"
-  
-    const killRick = document.getElementById("killRick")
-    killRick.innerText = "Game - Kill Rick"
-  
+
+
+    titleSkills.innerText = "My skills";
+
+    const titleProjects = document.getElementById("titleProjects");
+
+    titleProjects.innerText = "Projects";
+
+    const calculator = document.getElementById("calculator");
+    calculator.innerText = "Calculator";
+
+    const clock = document.getElementById("clock");
+    clock.innerText = "Digital Watch";
+
+    const killRick = document.getElementById("killRick");
+    killRick.innerText = "Game - Kill Rick";
+
     const projectLinks = document.querySelectorAll(".projectLink");
-  
-    projectLinks.forEach(link => {
-      link.textContent = "Project Link"
+
+    projectLinks.forEach((link) => {
+      link.textContent = "Project Link";
     });
-  
+
     const underConstruction = document.querySelectorAll(".underConstruction");
-  
-    underConstruction.forEach(text => {
-      text.textContent = "Under Constuction"
+
+    underConstruction.forEach((text) => {
+      text.textContent = "Under Constuction";
     });
 
-    const contacInformation = document.getElementById("contacInformation")
-    contacInformation.innerText ="Contact information:"
+    const contacInformation = document.getElementById("contacInformation");
+    contacInformation.innerText = "Contact information:";
 
-    const name = document.getElementById("name")
-    name.innerText = "Name: Gabriel Cardia"
-    translated = true
-  }});
+    const name = document.getElementById("name");
+    name.innerText = "Name: Gabriel Cardia";
+    translated = true;
+  }
+});
 
+let translated2 = false;
+const translateBtn2 = document.getElementById("translate2");
+translateBtn2.addEventListener("click", function () {
+  if (translated2) {
+    // revert the translations to the original state
+    translateBtn2.innerText = "EN";
+    const i1 = document.createElement("i");
+    i1.className = "fa-solid" + " " + "fa-earth-americas";
+    i1.style.marginLeft = "5px";
+    translateBtn2.appendChild(i1);
 
-  let translated2 = false;
-  const translateBtn2 = document.getElementById("translate2")
-  translateBtn2.addEventListener("click", function(){
-    if (translated2) {
-      // revert the translations to the original state
-      translateBtn2.innerText = "EN"
-      const i1 = document.createElement("i")
-      i1.className = "fa-solid" + " " + "fa-earth-americas"
-      i1.style.marginLeft = "5px"
-      translateBtn2.appendChild(i1)
-  
-      const contactHeader = document.getElementById("liveToastBtn")
-      contactHeader.innerText = "Contato" 
-      const i = document.createElement("i")
-      i.className = "fa-solid" + " " + "fa-phone"
-      i.style.marginLeft = "5px"
-      contactHeader.appendChild(i)
-  
-      const aboutTitle = document.getElementById("aboutTitle")
-      aboutTitle.innerText = "Sobre"
-  
-      const aboutText = document.getElementById("aboutText")
-      aboutText.innerText = "Olá, meu nome é Gabriel Cardia, tenho 20 anos. Atualmente, estou no 3º Semestre estudando Análise e Desenvolvimento de Sistemas. Sou apaixonado por programação e estou sempre aprendendo. Meu objetivo é se tornar um desenvolvedor FullStack. Faço cursos online, como o FullStack da OneBitcode e o Udemy: Complete Web Development - 20 cursos + 20 projetos."
-  
-      const titleSkills = document.getElementById("titleSkills")
-  
-      titleSkills.innerText = "Minhas Skills"
-  
-      const titleProjects = document.getElementById("titleProjects")
-  
-      titleProjects.innerText = "Projetos"
-  
-      const calculator = document.getElementById("calculator")
-      calculator.innerText = "Calculadora"
-  
-      const clock= document.getElementById("clock")
-      clock.innerText = "Relógio Digital"
-  
-      const killRick = document.getElementById("killRick")
-      killRick.innerText = "Jogo - Mate Rick"
-  
-      const projectLinks = document.querySelectorAll(".projectLink");
-  
-      projectLinks.forEach(link => {
-        link.textContent = "Link do Projeto"
-      });
-       
-      const underConstruction = document.querySelectorAll(".underConstruction");
+    const contactHeader = document.getElementById("liveToastBtn");
+    contactHeader.innerText = "Contato";
+    const i = document.createElement("i");
+    i.className = "fa-solid" + " " + "fa-phone";
+    i.style.marginLeft = "5px";
+    contactHeader.appendChild(i);
+
+    const aboutTitle = document.getElementById("aboutTitle");
+    aboutTitle.innerText = "Sobre";
     
-      underConstruction.forEach(text => {
-        text.textContent = "Em construção"
-      });
-  
-      const contacInformation = document.getElementById("contacInformation")
-      contacInformation.innerText ="Informaçãoes de contato:"
-  
-      const name = document.getElementById("name")
-      name.innerText = "Nome: Gabriel Cardia"
-  
-      translated2 = false;
-    } else {
-      // translate to English
-      translateBtn2.innerText = "PT"
-      const i1 = document.createElement("i")
-      i1.className = "fa-solid" + " " + "fa-earth-americas"
-      i1.style.marginLeft = "5px"
-      translateBtn2.appendChild(i1)
-      const contactHeader = document.getElementById("liveToastBtn")
-      contactHeader.innerText = "Contact" 
-      const i = document.createElement("i")
-      i.className = "fa-solid" + " " + "fa-phone"
-      i.style.marginLeft = "5px"
-      contactHeader.appendChild(i)
-  
-      const aboutTitle = document.getElementById("aboutTitle")
-      aboutTitle.innerText = "About"
-  
-      const aboutText = document.getElementById("aboutText")
-      aboutText.innerText = "Hello, my name is Gabriel Cardia, I am 20 years old. Currently, I am in the 3rd Semester studying Analysis and Development of Systems. I am passionate about programming and am constantly learning. I aim to become a FullStack developer. I take online courses, such as FullStack from OneBitcode and Udemy: Complete Web Development - 20 courses + 20 projects."
-      const titleSkills = document.getElementById("titleSkills")
-  
-      titleSkills.innerText = "My skills"
+    const aboutText = document.getElementById("aboutText");
+    aboutText.innerHTML =
+    "Olá, meu nome é Gabriel Cardia e eu tenho 20 anos. Atualmente, estou no 3º semestre do curso de Análise e Desenvolvimento de Sistemas e sou apaixonado por programação. Estou em constante aprendizado e pretendo me tornar um desenvolvedor FullStack. <br/><br/> Faço cursos online para aprimorar minhas habilidades, sendo eles:<br/><strong>- Udemy: Desenvolvimento Web Completo - 20 cursos + 20 projetos</strong><br/><strong>- FullStack da OneBitCode</strong>";
     
-      const titleProjects = document.getElementById("titleProjects")
-    
-      titleProjects.innerText = "Projects"
-    
-      const calculator = document.getElementById("calculator")
-      calculator.innerText = "Calculator"
-    
-      const clock= document.getElementById("clock")
-      clock.innerText = "Digital Watch"
-    
-      const killRick = document.getElementById("killRick")
-      killRick.innerText = "Game - Kill Rick"
-    
-      const projectLinks = document.querySelectorAll(".projectLink");
-    
-      projectLinks.forEach(link => {
-        link.textContent = "Project Link"
-      });
-    
-      const underConstruction = document.querySelectorAll(".underConstruction");
-    
-      underConstruction.forEach(text => {
-        text.textContent = "Under Constuction"
-      });
-  
-      const contacInformation = document.getElementById("contacInformation")
-      contacInformation.innerText ="Contact information:"
-  
-      const name = document.getElementById("name")
-      name.innerText = "Name: Gabriel Cardia"
-      translated2 = true
-    }});
-  
+      
+    const titleSkills = document.getElementById("titleSkills");
+
+    titleSkills.innerText = "Minhas Skills";
+
+    const titleProjects = document.getElementById("titleProjects");
+
+    titleProjects.innerText = "Projetos";
+
+    const calculator = document.getElementById("calculator");
+    calculator.innerText = "Calculadora";
+
+    const clock = document.getElementById("clock");
+    clock.innerText = "Relógio Digital";
+
+    const killRick = document.getElementById("killRick");
+    killRick.innerText = "Jogo - Mate Rick";
+
+    const projectLinks = document.querySelectorAll(".projectLink");
+
+    projectLinks.forEach((link) => {
+      link.textContent = "Link do Projeto";
+    });
+
+    const underConstruction = document.querySelectorAll(".underConstruction");
+
+    underConstruction.forEach((text) => {
+      text.textContent = "Em construção";
+    });
+
+    const contacInformation = document.getElementById("contacInformation");
+    contacInformation.innerText = "Informaçãoes de contato:";
+
+    const name = document.getElementById("name");
+    name.innerText = "Nome: Gabriel Cardia";
+
+    translated2 = false;
+  } else {
+    // translate to English
+    translateBtn2.innerText = "PT";
+    const i1 = document.createElement("i");
+    i1.className = "fa-solid" + " " + "fa-earth-americas";
+    i1.style.marginLeft = "5px";
+    translateBtn2.appendChild(i1);
+    const contactHeader = document.getElementById("liveToastBtn");
+    contactHeader.innerText = "Contact";
+    const i = document.createElement("i");
+    i.className = "fa-solid" + " " + "fa-phone";
+    i.style.marginLeft = "5px";
+    contactHeader.appendChild(i);
+
+    const aboutTitle = document.getElementById("aboutTitle");
+    aboutTitle.innerText = "About";
+
+    const aboutText = document.getElementById("aboutText");
+    aboutText.innerHTML = "Hello, my name is Gabriel Cardia and I'm 20 years old. Currently, I'm in the 3rd semester of the Analysis and Development of Systems course and I'm passionate about programming. I'm constantly learning and aiming to become a FullStack developer.<br><br>I take online courses to improve my skills, including:<br/><strong>- Udemy: Complete Web Development - 20 courses + 20 projects</strong><br/><strong>- OneBitCode's FullStack course</strong>";
+ 
+ 
+    const titleSkills = document.getElementById("titleSkills");
+
+    titleSkills.innerText = "My skills";
+
+    const titleProjects = document.getElementById("titleProjects");
+
+    titleProjects.innerText = "Projects";
+
+    const calculator = document.getElementById("calculator");
+    calculator.innerText = "Calculator";
+
+    const clock = document.getElementById("clock");
+    clock.innerText = "Digital Watch";
+
+    const killRick = document.getElementById("killRick");
+    killRick.innerText = "Game - Kill Rick";
+
+    const projectLinks = document.querySelectorAll(".projectLink");
+
+    projectLinks.forEach((link) => {
+      link.textContent = "Project Link";
+    });
+
+    const underConstruction = document.querySelectorAll(".underConstruction");
+
+    underConstruction.forEach((text) => {
+      text.textContent = "Under Constuction";
+    });
+
+    const contacInformation = document.getElementById("contacInformation");
+    contacInformation.innerText = "Contact information:";
+
+    const name = document.getElementById("name");
+    name.innerText = "Name: Gabriel Cardia";
+    translated2 = true;
+  }
+});
